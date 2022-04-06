0604d8a7e4583000331b6677a11a75f898fc99f7
taikyunn <taichi_141@me.com>
2022-04-06 16:36:59 +0900

    ?の使い方
    ?は値があってもなくてもOKという時に使用する。

--- dist/interface.js
+++ dist/interface.js
@@ -1,4 +1,8 @@
 "use strict";
+// nicknameフィールドがなくてもエラーにならない。
+const nameable = {
+    name: 'Tom',
+};
 const human = {
     name: 'Tom',
     age: 38,
@@ -10,13 +14,33 @@ const human = {
 // interfaceを実装するために使うのがimplements
 // implementsは複数つけることができる
 class Developer {
-    constructor(name, age, experience) {
+    // ?を使ったパラメーターは必要なパラメーターの後に書くこと。
+    constructor(name, age, experience, initSex) {
         this.name = name;
         this.age = age;
         this.experience = experience;
+        this.initSex = initSex;
+        // もし初期化する値があったら初期化・なかったらundefinedにする処理
+        if (initSex) {
+            this.sex = initSex;
+        }
     }
     greeting(message) {
         console.log('hello');
+    }
+    // パラメーターに?をつけることであってもなくてもいいを表現できる。
+    greeting2(message) {
+        if (message) {
+            message.toUpperCase();
+        }
+        console.log('hello');
+    }
+    // デフォルト引数:引数がなかった時にデフォルトで設定できるもの
+    greeting3(message = 'test') {
+        if (message) {
+            message.toUpperCase();
+        }
+        console.log(message);
     }
 }
 Developer.id = 0;
@@ -34,7 +58,7 @@ const user1 = {
         console.log(message);
     }
 };
-const developer = new Developer('Quill', 38, 3);
+const developer = new Developer('Quill', 38, 3, 'tom');
 let addFunc;
 addFunc = (n1, n2) => {
     return n1 + n2;
@@ -43,3 +67,6 @@ let addFunc1;
 addFunc1 = (n1, n2) => {
     return n1 + n2;
 };
+const developer4 = new Developer('Quill', 38, 3, 'tom');
+// デフォルト引数が指定されている場合は引数なしでもエラーにならない。
+console.log(developer4.greeting3());
--- src/interface.ts
+++ src/interface.ts
@@ -2,7 +2,15 @@
 // クラスと違い複数継承できる。
 interface Nameable {
   name: string;
+  // ?をつけることであってもなくてもいいという指定ができる。
+  // stringとundefined型が定義される。ただしstringとundefined型のユニオン型で定義するとエラーになる
+  nickname?: string
 }
+
+// nicknameフィールドがなくてもエラーにならない。
+const nameable: Nameable = {
+  name: 'Tom',
+};
 
 // interfaceの作成
 // typeエイリアスと同じようなもの。ほとんど同じ
@@ -15,6 +23,9 @@ interface Human extends Nameable {
   readonly age: number;
   // interfaceのメソッドの書き方
   greeting(message: string): void;
+
+  // メソッドにも?をつけることができる
+  greeting1?(message: string): void;
 }
 
 const human = {
@@ -29,10 +40,34 @@ const human = {
 // interfaceを実装するために使うのがimplements
 // implementsは複数つけることができる
 class Developer implements Human {
+  // class内でもフィールドに?を与えることで初期化しなくてもいいフィールドを設定できる
+  sex?: string;
   static id: number = 0;
-  constructor(public name: string, public age: number, public experience: number) {}
+  // ?を使ったパラメーターは必要なパラメーターの後に書くこと。
+  constructor(public name: string, public age: number, public experience: number, public initSex?: string) {
+    // もし初期化する値があったら初期化・なかったらundefinedにする処理
+    if (initSex) {
+      this.sex = initSex;
+    }
+  }
   greeting(message: string): void {
     console.log('hello');
+  }
+
+  // パラメーターに?をつけることであってもなくてもいいを表現できる。
+  greeting2(message?: string): void {
+    if (message) {
+      message.toUpperCase();
+    }
+    console.log('hello');
+  }
+
+  // デフォルト引数:引数がなかった時にデフォルトで設定できるもの
+  greeting3(message: string = 'test'): void {
+    if (message) {
+      message.toUpperCase();
+    }
+    console.log(message);
   }
 }
 
@@ -40,6 +75,7 @@ class Developer implements Human {
 // ただしclass内で指定したexperienceに関してはuserからアクセスできない。
 // 構造的部分型という
 const user: Human = new Developer('Quill', 38, 3);
+
 // readonlyなので上書き不可
 // user.age = 28;
 
@@ -52,7 +88,7 @@ const user1: Human = {
   }
 };
 
-const developer: Human = new Developer('Quill', 38, 3);
+const developer: Human = new Developer('Quill', 38, 3, 'tom');
 
 
 type addFunc = (num1: number, num2: number) => number;
@@ -72,3 +108,8 @@ let addFunc1: addFunc1;
 addFunc1 = (n1: number, n2: number) => {
   return n1 + n2;
 }
+
+const developer4 = new Developer('Quill', 38, 3, 'tom');
+
+// デフォルト引数が指定されている場合は引数なしでもエラーにならない。
+console.log(developer4.greeting3());
